f6438d7b799eedd2f8536b781f7bfddf7a556e28
João Victor Duarte <joao.victor@opovo.com.br>
2024-05-12 01:44:14 -0300

    fix: remove unused lib

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import svgr from 'vite-plugin-svgr'
 import tsconfigPaths from 'vite-tsconfig-paths'
-import Unfonts from 'unplugin-fonts/vite'
 import { resolve } from 'path'
 
 // https://vitejs.dev/config/
